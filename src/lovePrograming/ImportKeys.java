29104ed8f1a259b97fd06691e9c9797e869a677f
https://github.com/Rajeshwari10Bligar/Import.git <rajeshwaribaligar@gnail.com>
2019-01-07 22:52:55 +0000

    importprogram

---
+++ src/lovePrograming/ImportKeys.java
@@ -0,0 +1,28 @@
+package lovePrograming;
+
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class ImportKeys {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int []b={1,2,3,4,5,6};
+        int []c;
+        c=new int[]{1,2,3,4,5,6,7};
+        char d[];
+        d=new char[]{'a','b','c'};
+        //d=new char[]{f};try this one in home
+        //Random r = new Random();
+//        Date d ;
+        //java.util.Date da;
+       // java.sql.DriverManager driverManager;
+        System.out.println("3rd index value="+d[2]);
+         System.out.println("length of a array is="+a[0]);
+
+        MyClass abc = new MyClass();
+    }
+
+
+    private static class MyClass {
+    }
+}
